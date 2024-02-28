1c082deab32a74aefc06a79b472a139b50da0376
HWT0602 <103716012+HWT0602@users.noreply.github.com>
2024-02-28 14:25:18 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const hamburger = document.querySelector('#hamburger')
 const menu = document.querySelector('#menu')
+const more = document.querySelector('.hide-menu')
 
 const item1 = document.querySelector('.hide-button1')
 const item2 = document.querySelector('.hide-button2')
 const item3 = document.querySelector('.hide-button3')
 const item4 = document.querySelector('.hide-button4')
 const item5 = document.querySelector('.hide-button5')
+const item6 = document.querySelector('.hide-button6')
 
 hamburger.addEventListener('click', function () {
     menu.classList.toggle('hide')
@@ -25,6 +27,9 @@ item4.addEventListener('click', function () {
 })
 item5.addEventListener('click', function () {
     menu.classList.add('hide')
+})
+item6.addEventListener('click', function () {
+    more.classList.toggle('hide')
 })
 
 let pic1 = document.querySelector('.pic1')
